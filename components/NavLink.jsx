04ec4910ae66076c04149a028dd3f89e09039258
Ayodeji <drealayodeji@icloud.com>
2022-05-07 03:11:16 +0100

    Fixed layout

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavLink = ({ children, link }) => {
   return (
-    <div className="w-auto flex-shrink-0 text-sm px-4 py-[3px] border-[1.5px] border-primary rounded-2xl hover:scale-90 hover:bg-primary hover:text-white transition-all">
+    <div className="w-auto flex-shrink-0 text-xs text-primary font-semibold px-4 py-[3px] border-[1.5px] border-primary rounded-2xl hover:scale-90 hover:bg-primary hover:text-white transition-all">
       <Link href={link}>{children}</Link>
     </div>
   );
