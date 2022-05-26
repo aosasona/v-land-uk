453983b83c6db909db5a2866fbe0a242459a8a33
Ayodeji <drealayodeji@icloud.com>
2022-05-26 20:26:08 +0100

    Added comments fetching

--- components/CommentsArea.jsx
+++ components/CommentsArea.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import Moment from "react-moment";
+import { API } from "../config/api";
+const qs = require("qs");
 
 const CommentsArea = ({ id, comments, setComments }) => {
+  const filter = qs.stringify({
+    filters: {
+      articleId: {
+        $eq: id,
+      },
+    },
+    populate: "*",
+  });
+
+  useEffect(() => {
+    const url = `${API}/feedbacks?${filter}`;
+    const fetchData = async () => {
+      const res = await axios.get(url);
+      setComments(res.data.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <section className="px-2">
       <h1 className="text-3xl lg:text-4xl">Comments</h1>
@@ -10,7 +32,27 @@ const CommentsArea = ({ id, comments, setComments }) => {
             No comments.
           </div>
         ) : (
-          <div></div>
+          comments.map((comment, index) => (
+            <div
+              key={index}
+              className="bg-primary bg-opacity-[0.05] py-4 px-5 mb-2 lg:mb-3 rounded-lg"
+            >
+              <div className="flex gap-x-1">
+                <p className="text-xs tracking-wide">
+                  {comment?.attributes?.name}
+                </p>
+                <Moment
+                  format="MMM Do YYYY"
+                  className="text-[10px] text-primary text-opacity-70 poppins"
+                >
+                  {comment?.attributes?.createdAt || Date.now()}
+                </Moment>
+              </div>
+              <p className="text-primary text-[12px] lg:text-sm poppins tracking-wide leading-normal mt-2 mb-0">
+                {comment?.attributes?.comment}
+              </p>
+            </div>
+          ))
         )}
       </div>
     </section>
