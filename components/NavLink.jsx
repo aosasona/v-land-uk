d78728fb53897fb13d2733ff51b0f03cf1f9191a
Ayodeji <drealayodeji@icloud.com>
2022-05-27 04:15:07 +0100

    Fixed navlink

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -3,9 +3,11 @@ import React from "react";
 
 const NavLink = ({ children, link }) => {
   return (
-    <div className="w-auto flex-shrink-0 text-xs text-primary font-semibold px-4 py-[3px] border-[1.5px] border-primary rounded-2xl hover:scale-90 hover:bg-primary hover:text-white transition-all">
-      <Link href={link}>{children}</Link>
-    </div>
+    <Link href={link} passHref>
+      <div className="w-auto flex-shrink-0 text-xs text-primary font-semibold px-4 py-[3px] border-[1.5px] border-primary rounded-2xl hover:scale-90 hover:bg-primary hover:text-white transition-all">
+        {children}
+      </div>
+    </Link>
   );
 };
 
