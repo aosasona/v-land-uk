a3960529e587b6154891229f6e7fa358b96cd64b
Ayodeji <drealayodeji@icloud.com>
2022-05-28 04:02:23 +0100

    Added teams page

--- pages/team.js
+++ pages/team.js
@@ -42,7 +42,7 @@ const Team = ({ excerpt, team }) => {
                 className="w-full flex flex-col gap-2 bg-white rounded-xl drop-shadow-md  mb-4"
               >
                 <div className="w-full grid grid-cols-2 items-center gap-3 lg:gap-5 px-4 lg:px-7 py-5">
-                  <div className="w-[90%] lg:w-[90%] aspect-square rounded-full">
+                  <div className="w-[90%] lg:w-[90%] aspect-square rounded-full overflow-hidden">
                     <img
                       src={
                         staff?.attributes?.image?.data?.attributes?.formats
