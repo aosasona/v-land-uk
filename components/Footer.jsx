58b81e5737ca81e0bd65037e30af447e0512fed7
Ayodeji <drealayodeji@icloud.com>
2022-06-10 21:45:59 +0100

    Update: layout changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -97,12 +97,12 @@ const Footer = () => {
 
         {/* Email subscriptions */}
         <div className="lg:w-[60%] 2xl:w-3/6">
-          <h2 className="text-4xl lg:text-4xl text-center lg:text-left mb-1 px-1 lg:px-2">
-            Subscribe
+          <h2 className="text-4xl lg:text-4xl font-[800] text-center lg:text-left mb-1 px-1 lg:px-2">
+            Newsletter
           </h2>
-          <h4 className="w-[95%] mx-auto text-primary text-opacity-70 text-[11px] lg:text-xs font-normal tracking-wider text-center lg:text-left my-5">
-            Subscribe to our newsletter to receive the latest news and updates
-            directly from us in your inbox. Best part? It&apos;s free!
+          <h4 className="w-[95%] mx-auto text-primary text-opacity-80 text-[11px] lg:text-xs font-normal tracking-wider text-center lg:text-left my-5">
+            We’ll keep you updated on new information, upcoming events, and the
+            best deals in your area.
           </h4>
           <form
             onSubmit={emailSubmit}
